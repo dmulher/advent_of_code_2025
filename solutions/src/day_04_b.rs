fb4673dab09744023483204b614e785253dec382
Dom Mulherin <dom.mulherin@proptrack.com>
2025-12-05 08:44:54 +1000

    Off by 2

--- solutions/src/day_04_b.rs
+++ solutions/src/day_04_b.rs
@@ -1,6 +1,6 @@
 extern crate test;
 
-pub fn main(contents: String) -> u32 {
+pub fn main(contents: String) -> u16 {
   get_toilet_papers(contents)
 }
 
@@ -10,113 +10,127 @@ enum Spot {
   Roll(u8)
 }
 
-fn get_toilet_papers(contents: String) -> u32 {
-  let mut map = contents
+fn get_toilet_papers(contents: String) -> u16 {
+  let mut map = create_map(contents);
+  let initial_rolls = map.iter().map(|line| line.iter().map(|x| *x as u16).sum::<u16>()).sum::<u16>();
+  map = set_initial_adjacency(map);
+  let ans = remove_rolls(&mut map);
+  let final_rolls = map.iter().map(|line| line.iter().map(|x| if x > &0 {1u16} else {0u16}).sum::<u16>()).sum::<u16>();
+  println!("{} - {} = {}", initial_rolls, final_rolls, initial_rolls-final_rolls);
+  ans
+}
+
+fn create_map(contents: String) -> Vec<Vec<u8>> {
+  contents
     .lines()
-    .map(|line| line.chars().map(|c| if c == '@' {Spot::Roll(0)} else {Spot::None}).collect::<Vec<Spot>>())
-    .collect::<Vec<Vec<Spot>>>();
-  let mut rolls: u32 = 0;
+    .map(|line| line.chars().map(|c| if c == '@' {1} else {0}).collect::<Vec<u8>>())
+    .collect::<Vec<Vec<u8>>>()
+}
+
+fn set_initial_adjacency(mut map: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
   for i in 0..map.len() {
     for j in 0..map[0].len() {
-      if map[i][j] == Spot::None {
+      if map[i][j] == 0 {
         continue;
       }
       let mut surrounding: u8 = 0;
       if i > 0 {
         if j > 0 {
-          if map[i-1][j-1] != Spot::None {
+          if map[i-1][j-1] > 0 {
             surrounding += 1;
           }
         }
-        if map[i-1][j] != Spot::None {
+        if map[i-1][j] > 0 {
           surrounding += 1;
         }
         if j < map[i].len() - 1 {
-          if map[i-1][j+1] != Spot::None {
+          if map[i-1][j+1] > 0 {
             surrounding += 1;
           }
         }
       }
       if j > 0 {
-        if map[i][j-1] != Spot::None {
+        if map[i][j-1] > 0 {
           surrounding += 1;
         }
       }
       if j < map[i].len() - 1 {
-        if map[i][j+1] != Spot::None {
+        if map[i][j+1] > 0 {
           surrounding += 1;
         }
       }
       if i < map.len() - 1 {
         if j > 0 {
-          if map[i+1][j-1] != Spot::None {
+          if map[i+1][j-1] > 0 {
             surrounding += 1;
           }
         }
-        if map[i+1][j] != Spot::None {
+        if map[i+1][j] > 0 {
           surrounding += 1;
         }
         if j < map[i].len() - 1 {
-          if map[i+1][j+1] != Spot::None {
+          if map[i+1][j+1] > 0 {
             surrounding += 1;
           }
         }
       }
-      if surrounding < 4 {
-        rolls += change_adjacency(&mut map, i, j, i, j);
-      } else {
-        map[i][j] = Spot::Roll(surrounding);
+      map[i][j] = surrounding;
+    }
+  }
+  map
+}
+
+fn remove_rolls(map: &mut Vec<Vec<u8>>) -> u16 {
+  let mut rolls_removed = 0u16;
+  for i in 0..map.len() {
+    for j in 0..map[0].len() {
+      if map[i][j] > 0 && map[i][j] < 4 {
+        rolls_removed += remove_roll(map, i, j);
       }
     }
   }
-  for line in map.iter() {
-    println!("{:?}", line);
-  }
-  rolls
+  rolls_removed
 }
 
-fn change_adjacency(map: &mut Vec<Vec<Spot>>, i: usize, j: usize, oi: usize, oj: usize) -> u32 {
-  map[i][j] = Spot::None;
-  let mut changes: u32 = 1;
-  if j > 0 {
-    changes += check_for_cascade(map, i, j-1, oi, oj);
-  }
+fn remove_roll(map: &mut Vec<Vec<u8>>, i: usize, j: usize) -> u16 {
+  map[i][j] = 0;
+  let mut rolls_removed = 1u16;
   if i > 0 {
     if j > 0 {
-      changes += check_for_cascade(map, i-1, j-1, oi, oj);
+      rolls_removed += check_for_cascade(map, i-1, j-1);
     }
-    changes += check_for_cascade(map, i-1, j, oi, oj);
+    rolls_removed += check_for_cascade(map, i-1, j);
     if j < map[i].len() - 1 {
-      changes += check_for_cascade(map, i-1, j+1, oi, oj);
+      rolls_removed += check_for_cascade(map, i-1, j+1);
     }
   }
-  if (i == oi && oj > 1 && j < oj - 2) || (i < oi && j < map[i].len() - 1) {
-    changes += check_for_cascade(map, i, j+1, oi, oj);
+  if j > 0 {
+    rolls_removed += check_for_cascade(map, i, j-1);
   }
-  if i < oi {
-    if j > 0 && (i < oi - 1 || j - 1 < oj) {
-      changes += check_for_cascade(map, i+1, j-1, oi, oj);
+  if j < map[i].len() - 1 {
+    rolls_removed += check_for_cascade(map, i, j+1);
+  }
+  if i < map.len() - 1 {
+    if j > 0 {
+      rolls_removed += check_for_cascade(map, i+1, j-1);
     }
-    if i < oi - 1 {
-      changes += check_for_cascade(map, i+1, j, oi, oj);
-    }
-    if (i < oi - 1 && j < map[i].len() - 1) || j < oj {
-      changes += check_for_cascade(map, i+1, j+1, oi, oj);
+    rolls_removed += check_for_cascade(map, i+1, j);
+    if j < map[i].len() - 1 {
+      rolls_removed += check_for_cascade(map, i+1, j+1);
     }
   }
-  changes
+  rolls_removed
 }
 
-fn check_for_cascade(map: &mut Vec<Vec<Spot>>, i: usize, j: usize, oi: usize, oj: usize) -> u32 {
-  let mut changes = 0u32;
-  if let Spot::Roll(x) = map[i][j] {
-    if x == 4 {
-      changes += change_adjacency(map, i, j, oi, oj);
-    } else {
-      map[i][j] = Spot::Roll(x - 1);
-    }
+fn check_for_cascade(map: &mut Vec<Vec<u8>>, i: usize, j: usize) -> u16 {
+  if map[i][j] > 0 && map[i][j] < 5 {
+    remove_roll(map, i, j)
+  } else if map[i][j] > 0 {
+    map[i][j] -= 1;
+    0
+  } else {
+    0
   }
-  changes
 }
 
 #[cfg(test)]
@@ -130,18 +144,18 @@ mod tests {
 
   #[test]
   fn test_day_04_b() {
-    const EXAMPLE_ANSWER: Option<u32> = Some(43);
-    const ANSWER: Option<u32> = None;
-    match utils::run_method::<u32>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
+    const EXAMPLE_ANSWER: Option<u16> = Some(43);
+    const ANSWER: Option<u16> = Some(9083);
+    match utils::run_method::<u16>(&main, DAY, PART, (EXAMPLE_ANSWER, ANSWER)) {
       Err(message) => panic!("{}", message),
       Ok(val) if ANSWER.is_none() => println!("Answer for day {DAY}-{} = {val}", PART.lower_name()),
       _ => (),
     }
   }
 
-  // #[bench]
-  // fn bench_day_04_b(b: &mut Bencher) {
-  //   let input = read_file_to_string(utils::get_file_name(DAY, None).as_str());
-  //   b.iter(|| main(input.clone()));
-  // }
+  #[bench]
+  fn bench_day_04_b(b: &mut Bencher) {
+    let input = read_file_to_string(utils::get_file_name(DAY, None).as_str());
+    b.iter(|| main(input.clone()));
+  }
 }
